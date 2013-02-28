05b214ea8481efecc1a6513b5bfe4800943dabf7
John <john@triplingo.com>
2013-02-28 14:20:32 -0500

    removed some stuff from my profile

--- modules/people/manifests/jcowhigjr.pp
+++ modules/people/manifests/jcowhigjr.pp
@@ -1,18 +1,17 @@
 ```puppet
 class people::jcowhigjr {
-  include mongodb
-  include emacs   # requires emacs module in Puppetfile
-  include sparrow # requires sparrow module in Puppetfile
-  include watts
-	include vlc 
-	include things 
-	include spotify 
-	include skype 
-	include propane 
-	include istatmenus3 
-	include flux 
-	include airfoil 
-	include istatmenus4
+#  include mongodb
+#  include emacs   # requires emacs module in Puppetfile
+#  include sparrow # requires sparrow module in Puppetfile
+#  include watts
+#	include things 
+#	include spotify 
+#	include skype 
+#	include propane 
+#	include istatmenus3 
+#	include flux 
+#	include airfoil 
+#	include istatmenus4
 
   $home     = "/Users/${::luser}"
   $my       = "${home}/my"
